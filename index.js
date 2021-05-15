20573eeaa53579e5d562a5c2b231001a858c0cf2
Angel <angelpena619@gmail.com>
2021-05-15 14:39:28 -0700

    adding readme file

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const generateHTML = (answers) =>
 <ul class="list-group list-group-flush">
     <li class="list-group-item">ID: ${answers.id}</li>
     <li class="list-group-item">Email Address: ${answers.email}</li>
-    <li class="list-group-item">School: ${answers.role}</li>
+    <li class="list-group-item">School: ${answers.school}</li>
 </ul>
 </div>
 </div>
@@ -30,45 +30,75 @@ const generateHTML = (answers) =>
 </body>
 </html>`;
 
+const newTeamMemberQuestions= [{
+  // type: 'input',
+  message: "Enter team member's name",
+  name: "name"
+},
+{
+  message: "Enter github",
+  name: "github",
+},
+{
+  type: "list",
+    message: "Select team member's role",
+    choices: 
+    ["Engineer",
+      "Intern",
+      "Manager"],
+    name: "role"
+},
+{
+  message: "Enter team member's id",
+  name: "id",
+},
+{
+  message: "Enter team member's email address",
+    name: "email"
+},
+{
+  message: "Enter School name",
+    name: "school"
+},
+{
+  type: "list",
+  message: "Would you like to add more team members?",
+  choices: 
+  ["yes",
+    "no"],
+    name: "moMembers"
+}];
 
-inquirer.prompt([{
-      // type: 'input',
-      message: "Enter team member's name",
-      name: "name"
-    },
-    {
-      message: "Enter github",
-      name: "github",
-    },
-    {
-      type: "list",
-        message: "Select team member's role",
-        choices: 
-        ["Engineer",
-          "Intern",
-          "Manager"],
-        name: "role"
-    },
-    {
-      message: "Enter team member's id",
-      name: "id",
-    },
-    {
-      message: "Enter team member's email address",
-        name: "email"
-    },
-    {
-      type: "list",
-      message: "Would you like to add more team members?",
-      choices: 
-      ["yes",
-        "no"],
-        name: "moMembers"
-    }])
-    .then((answers) => {
-      const htmlPageContent = generateHTML(answers);
-  
-      fs.writeFile('index.html', htmlPageContent, (err) =>
-        err ? console.log(err) : console.log('Successfully created your team!')
-      );
-    });
+const addTeamMember = async ()=>{
+  inquirer.prompt(newTeamMemberQuestions)
+  .then((answers) => {
+    console.log(answers);
+    if (answers.moMembers === "yes") {
+      teamMembers.push(answers);
+
+    return await  addTeamMember()
+    } else {
+      console.log(teamMembers);
+    }
+    
+  })
+}
+
+var teamMembers = []
+addTeamMember()
+.then(()=>{
+  for (i = 0; i < teamMembers.length; i++){
+    var currentTeamMember = teamMembers [0]
+    var htmlBucket = generateHTML (currentTeamMember)
+     fs.appendFile('index.html', htmlBucket, (err) =>
+       err ? console.log(err) : console.log('Successfully created your team!')
+     );
+  }
+})
+  // const htmlPageContent = generateHTML(answers);
+
+  // fs.writeFile('index.html', htmlPageContent, (err) =>
+  //   err ? console.log(err) : console.log('Successfully created your team!')
+  // );
+
+
